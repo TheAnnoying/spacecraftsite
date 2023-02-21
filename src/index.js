0ef727b85dc74898f1c4a47481090448def8437b
TheAnnoying <drork2018@gmail.com>
2023-02-21 22:10:06 +0200

    Added the addCosmetic function and include-html attribute

--- src/index.js
+++ src/index.js
@@ -16,3 +16,36 @@ function copyText(){
         }, 2500)
     }, 0)
 }
+
+document.querySelectorAll("[data-include-html]").forEach(async e => {
+    const r = await fetch(e.getAttribute("data-include-html"));
+    e.innerHTML = await r.text();
+});
+
+const cosmeticList = document.getElementById("cosmetic-list");
+
+function addCosmetic(args){
+    let element = document.createElement("div");
+    element.classList.add("bg-neutral", "p-10", "rounded-2xl", "hover:text-white", "duration-700", "hover:bg-base200");
+    element.innerHTML = `
+        <img src="${args.imgsrc}">
+        <h1 class="font-bold text-xl mt-10">${args.name}</h1>
+        <p class="mb-5">${args.description}</p>
+        <span class="bg-info p-2 rounded-xl text-info-content"><i class="fa-solid fa-coins"></i> ${args.cost} coins</span>
+    `
+    cosmeticList.append(element);
+}
+
+addCosmetic({
+    imgsrc: "/assets/cosmetics/camera.png",
+    name: "Camera Balloon",
+    description: "Go around with the Camera Balloon, say cheese!",
+    cost: 120
+});
+
+addCosmetic({
+    imgsrc: "/assets/cosmetics/regular_balloon.png",
+    name: "The regular old balloon, enjoy it while it's there",
+    description: "Go around with the Camera Balloon, say cheese!",
+    cost: 70
+});
